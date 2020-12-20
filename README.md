a1cbc084751389e07c1b0b470284f53f1dc0faef
subicura <subicura@subicura.com>
2020-12-20 22:58:23 +0900

    add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# shoplive guide
+
+## dev
+
+```
+yarn install
+yarn dev
+```
+
+http://localhost:8080
+
+## deploy
+
+```
+./deploy.sh
+```
--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:subicura/k8s.git master:gh-pages
+git push -f git@github.com:shoplive/guide.git master:gh-pages
 
 cd -
